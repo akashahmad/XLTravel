b6b84b7194bf8c588c5b8d62ea12972445521ecc
akashahmad <engr.akashahmad@gmail.com>
2022-03-03 20:26:19 +0500

    index page calendar issue resolved , changes in responsive , seachh bar mobile color changed

--- index.js
+++ index.js
@@ -249,6 +249,24 @@ const swiper = new Swiper(".swiper", {
   },*/
 });
 
+// Carousel swipe fuction for mobile
+$('.carousel').on('touchstart', function(event){
+  const xClick = event.originalEvent.touches[0].pageX;
+  $(this).one('touchmove', function(event){
+      const xMove = event.originalEvent.touches[0].pageX;
+      const sensitivityInPx = 5;
+
+      if( Math.floor(xClick - xMove) > sensitivityInPx ){
+          $(this).carousel('next');
+      }
+      else if( Math.floor(xClick - xMove) < -sensitivityInPx ){
+          $(this).carousel('prev');
+      }
+  });
+  $(this).on('touchend', function(){
+      $(this).off('touchmove');
+  });
+});
 
 // Display Block and none function
 function displayBlock(value){
