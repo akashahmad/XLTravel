3f6c8cc87499da51172e04761abf6ecf922110fd
akashahmad <engr.akashahmad@gmail.com>
2022-04-26 04:41:24 +0500

    few fixes to responsiveness of become-host page

--- utils/helpCenter.js
+++ utils/helpCenter.js
@@ -1,97 +1,95 @@
-
 // Host fiter for Hepl Center Page
-var iso = new Isotope( '.filter-host', {
-    itemSelector: '.element-item',
-    layoutMode: 'fitRows'
-  });
+var iso = new Isotope(".filter-host", {
+  itemSelector: ".element-item",
+  layoutMode: "fitRows",
+});
 
-  // filter functions
-  var filterFns = {
-    // show if name ends with -ium
-    ium: function( itemElem ) {
-      var name = itemElem.querySelector('.name').textContent;
-      return name.match( /ium$/ );
-    }
-  };
-  
-  // bind filter button click
-  var filtersElem = document.querySelector('.filters-button-group');
-  filtersElem.addEventListener( 'click', function( event ) {
+// filter functions
+var filterFns = {
+  // show if name ends with -ium
+  ium: function (itemElem) {
+    var name = itemElem.querySelector(".name").textContent;
+    return name.match(/ium$/);
+  },
+};
+
+// bind filter button click
+var filtersElem = document.querySelector(".filters-button-group");
+filtersElem.addEventListener("click", function (event) {
+  // only work with buttons
+  if (!matchesSelector(event.target, "button")) {
+    return;
+  }
+  var filterValue = event.target.getAttribute("data-filter");
+  // use matching filter function
+  filterValue = filterFns[filterValue] || filterValue;
+  iso.arrange({ filter: filterValue });
+});
+
+// change is-checked class on buttons
+var buttonGroups = document.querySelectorAll(".button-group");
+for (var i = 0, len = buttonGroups.length; i < len; i++) {
+  var buttonGroup = buttonGroups[i];
+  radioButtonGroup(buttonGroup);
+}
+function radioButtonGroup(buttonGroup) {
+  buttonGroup.addEventListener("click", function (event) {
     // only work with buttons
-    if ( !matchesSelector( event.target, 'button' ) ) {
+    if (!matchesSelector(event.target, "button")) {
       return;
     }
-    var filterValue = event.target.getAttribute('data-filter');
-    // use matching filter function
-    filterValue = filterFns[ filterValue ] || filterValue;
-    iso.arrange({ filter: filterValue });
+    buttonGroup.querySelector(".is-checked").classList.remove("is-checked");
+    event.target.classList.add("is-checked");
   });
-  
-  // change is-checked class on buttons
-  var buttonGroups = document.querySelectorAll('.button-group');
-  for ( var i=0, len = buttonGroups.length; i < len; i++ ) {
-    var buttonGroup = buttonGroups[i];
-    radioButtonGroup( buttonGroup );
-  }
-  function radioButtonGroup( buttonGroup ) {
-    buttonGroup.addEventListener( 'click', function( event ) {
-      // only work with buttons
-      if ( !matchesSelector( event.target, 'button' ) ) {
-        return;
-      }
-      buttonGroup.querySelector('.is-checked').classList.remove('is-checked');
-      event.target.classList.add('is-checked');
-    });
-  }
-  
+}
+
 // guest fiter for Hepl Center Page
 
 // init Isotope
-var isoGuest = new Isotope( '.filter-guest', {
-  itemSelector: '.element-item-guest',
-  layoutMode: 'fitRows'
+var isoGuest = new Isotope(".filter-guest", {
+  itemSelector: ".element-item-guest",
+  layoutMode: "fitRows",
 });
 
 // filter functions
 var filterFnsGroup = {
   // show if name ends with -ium
-  ium: function( itemElem ) {
-    var name = itemElem.querySelector('.name').textContent;
-    return name.match( /ium$/ );
-  }
+  ium: function (itemElem) {
+    var name = itemElem.querySelector(".name").textContent;
+    return name.match(/ium$/);
+  },
 };
 
 // bind filter button click
-var filtersElemGuest = document.querySelector('.filters-button-group-guest');
+var filtersElemGuest = document.querySelector(".filters-button-group-guest");
 
-filtersElemGuest.addEventListener( 'click', function( event ) {
+filtersElemGuest.addEventListener("click", function (event) {
   // only work with buttons
-  if ( !matchesSelector( event.target, 'button' ) ) {
+  if (!matchesSelector(event.target, "button")) {
     return;
   }
-  var filterValue = event.target.getAttribute('data-filter-guest');
+  var filterValue = event.target.getAttribute("data-filter-guest");
   // use matching filter function
-  filterValue = filterFnsGroup[ filterValue ] || filterValue;
+  filterValue = filterFnsGroup[filterValue] || filterValue;
   isoGuest.arrange({ filter: filterValue });
 });
 
 // change is-checked class on buttons
-var buttonGroups = document.querySelectorAll('.button-group-guest');
-for ( var i=0, len = buttonGroups.length; i < len; i++ ) {
+var buttonGroups = document.querySelectorAll(".button-group-guest");
+for (var i = 0, len = buttonGroups.length; i < len; i++) {
   var buttonGroup = buttonGroups[i];
-  radioButtonGroup( buttonGroup );
+  radioButtonGroup(buttonGroup);
 }
-function radioButtonGroup( buttonGroup ) {
-  buttonGroup.addEventListener( 'click', function( event ) {
+function radioButtonGroup(buttonGroup) {
+  buttonGroup.addEventListener("click", function (event) {
     // only work with buttons
-    if ( !matchesSelector( event.target, 'button' ) ) {
+    if (!matchesSelector(event.target, "button")) {
       return;
     }
-    buttonGroup.querySelector('.is-checked').classList.remove('is-checked');
-    event.target.classList.add('is-checked');
+    buttonGroup.querySelector(".is-checked").classList.remove("is-checked");
+    event.target.classList.add("is-checked");
   });
 }
-
 
 // Yellow button Add Class For Help Center Page
 function addYellowBg(value) {
