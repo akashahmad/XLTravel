f39d7b082671d13e69330ec86bfc46d65af47c22
akashahmad <engr.akashahmad@gmail.com>
2022-05-17 19:06:53 +0500

    section width issue solved with responsive

--- utils/publicReview.js
+++ utils/publicReview.js
@@ -1,14 +1,13 @@
 var popup = document.getElementById("popup");
-// var scroll =   
+// var scroll =
 function showModal() {
   if (popup.classList.contains("d-none")) {
     popup.classList.remove("d-none");
-    document.body.style.overflow = 'hidden';
+    document.body.style.overflow = "hidden";
     active[i].classList.add("display-block");
   }
 }
 function hideModal() {
   popup.classList.add("d-none");
-  document.body.style.overflow = 'visible';
-
+  document.body.style.overflow = "visible";
 }
